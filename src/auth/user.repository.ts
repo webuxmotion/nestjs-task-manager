2816633a52af0131b11f3cead095272b78ac651e
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-10-27 20:54:51 +0200

    Auth user. Save to database without hashing the password

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,7 +1,27 @@
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 import { EntityRepository, Repository } from "typeorm";
+import { AuthCredentialsDto } from "./dto/auth-credentials.dto";
 import { User } from "./user.entity";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
 
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = new User();
+    
+    user.username = username;
+    user.password = password;
+    
+    try {
+      await user.save();
+    } catch (error) {
+      console.log('error.code', error.code);
+      if (error.code === '23505') { // duplicate username
+        throw new ConflictException(`Username "${username}" already exists`);
+      } else {
+        throw new InternalServerErrorException();
+      }
+    }
+  }
 }
