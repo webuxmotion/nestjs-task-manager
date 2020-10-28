0ebd494aac395af0903cdb6c88df88b86d7bc375
Andrii Pereverziev <andriipereverziev@Andriis-MacBook-Pro.local>
2020-10-28 11:12:48 +0200

    Pre-production package.json adjustments

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,13 @@ async function bootstrap() {
   const port = process.env.PORT || serverConfig.port;
   const app = await NestFactory.create(AppModule);
 
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+    logger.log(`Accepting requests from origin "${serverConfig.origin}"`);
+  }
+  
   await app.listen(port);
   logger.verbose(`Application listening on port ${port}`);
 }
